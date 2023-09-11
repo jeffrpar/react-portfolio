92f3448a904da533c7b02517771e15f1ee265d12
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-11 18:28:51 -0500

    added resume component

---
+++ src/components/Main/Resume/Resume.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Resume.css';
+
+function Resume() {
+  return (
+    <section className="resume">
+      <div className="resume-content">
+        <h2>Resume</h2>
+        <div className="proficiencies">
+          <h3>Proficiencies</h3>
+          <ul>
+            <li>Web Development (HTML, CSS, JavaScript)</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
