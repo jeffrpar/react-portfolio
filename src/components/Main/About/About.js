65bb0d076e953ee04e603583d77b736095ed33b4
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-11 15:22:06 -0500

    added About component and styling

---
+++ src/components/Main/About/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './About.css'; // Import your CSS file for styling
+
+function About() {
+  return (
+    <section className="about">
+      <div className="about-content">
+        {/* Add your photo or avatar here */}
+        <img
+          src="your-photo-url.jpg"
+          alt="Your Name"
+          className="about-photo"
+        />
+
+        <div className="about-text">
+          <h2>About Me</h2>
+          <p>
+            {/* Add a short bio about yourself */}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            bibendum magna non ex volutpat, et dapibus nisl fringilla.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
