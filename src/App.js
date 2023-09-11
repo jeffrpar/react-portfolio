b80edbe9b137a68967326a2c86365c0343e4b8e9
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-09-11 14:19:52 -0500

    edited App.js to display components based on useState

--- src/App.js
+++ src/App.js
@@ -6,3 +6,32 @@ import Projects from './components/Main/Projects/Projects';
 import Contact from './components/Main/Contact/Contact';
 import Resume from './components/Main/Resume/Resume';
 import Footer from './components/Footer/Footer';
+
+function App() {
+  // Define state to track the active section
+  const [activeSection, setActiveSection] = useState('About'); // Default to 'About'
+
+  // Function to change the active section based on user selection
+  const handleSectionChange = (sectionName) => {
+    setActiveSection(sectionName);
+  };
+
+  return (
+    <div className="App">
+      <Header>
+        {/* Nest Nav within Header and pass a function to update the active section */}
+        <Nav onSectionChange={handleSectionChange} activeSection={activeSection} />
+      </Header>
+
+      {/* Conditionally render the selected Main component */}
+      {activeSection === 'About' && <About />}
+      {activeSection === 'Projects' && <Projects />}
+      {activeSection === 'Contact' && <Contact />}
+      {activeSection === 'Resume' && <Resume />}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
